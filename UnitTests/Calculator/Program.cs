781617c5f8f7148b458e4f1b8bd96d39f430fbc3
Kokhanchik Anton <kokhanchik.tversu@gmail.com>
2016-03-18 20:57:49 +0300

    proram and tests writed

--- UnitTests/Calculator/Program.cs
+++ UnitTests/Calculator/Program.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Calculator
 {
 	class Program
 	{
-
 		static void Main(string[] args)
 		{
+			Controller c = new Controller();
+			do { }
+			while (c.Menu());
 		}
+
+		
 	}
 }
